ec32db3dce8881525649ffd208f5ac3ca1cd4651
Dyltia27 <162216536+Dyltia27@users.noreply.github.com>
2024-05-09 18:23:36 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AnimalComponent } from './components/animal/animal.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AnimalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  
+}
---
+++ src/app/services/animal.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimalService {
+  apiUri = '/api/animals';
+
+  constructor(private http: HttpClient) {}
+
+  getAllAnimalsData(): Observable<any> {
+
+    return this.http.get(this.apiUri, {
+      headers:
+      {
+        'Content-Type': 'application/json',
+      }
+    });
+  }
+
+  newAnimal(data: any): Observable<any> {
+    return this.http.post<any>(
+      this.apiUri,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+  }
+
+  updateAnimal(id: any, data: any): Observable<any> {
+    console.log(data)
+    return this.http.put<any>(
+      this.apiUri + '/' + id,
+      data,
+      { headers: {
+        'Content-Type': 'application/json',
+      } });
+  }
+
+  getOneAnimal(id: any): Observable<any> {
+    return this.http.get<any>(
+      this.apiUri + '/' + id,
+      { headers: {
+        'Content-Type': 'application/json',
+      } });
+  }
+
+  deleteAnimal(id: any) {
+    return this.http.delete<any>(
+      this.apiUri + "/" + id,
+      { headers: {
+        'Content-Type': 'application/json',
+      } });
+  }
+
+}
